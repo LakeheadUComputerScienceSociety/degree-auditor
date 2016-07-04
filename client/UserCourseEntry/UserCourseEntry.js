e6fb40e70e7ca0c9fee047945610f56351cd78b0
ErikTillberg <eatillberg@gmail.com>
2016-07-04 19:09:13 -0400

    user course entry page pulls from degree_requirements and filters the courses into their proper years.

--- client/UserCourseEntry/UserCourseEntry.js
+++ client/UserCourseEntry/UserCourseEntry.js
@@ -9,15 +9,23 @@ Template.UserCourseEntry.helpers({
    student_courses: function(){
      return StudentCourses.find({});
    },
+   degree_courses: function(){
+     return DegreeReqs.find({});
+   },
    concat: function(a, b){
      return a + ' ' + b;
-   }
+   },
+   isYear: function(year, courseNum){ //returns if the course number matches the year (e.g. 1411 matches 1)
+     console.log(courseNum);
+     return year.toString() == courseNum.toString().charAt(0);
+  }
 });
 
 Template.UserCourseEntry.onCreated(function(){
     var self = this;
     self.autorun(function(){
-         self.subscribe("luCourses");
+         //self.subscribe("luCourses");
          self.subscribe("studentCourses");
+         self.subscribe("degreeReqs");
     });
 });
